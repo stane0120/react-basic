9ef0109c44d61a5d1ff5d88ad25cd9f2312dc962
DESKTOP-O3LMC10\User <hsim0203@naver.com>
2024-05-14 14:23:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>내가 가는 이 길이 어디로 가는지 
+           <br/>어디로 날 데려가는지 그 곳은 어딘지</h2>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
