356a679ee4939fba765c68cca3018c9229e45caf
DESKTOP-O3LMC10\User <hsim0203@naver.com>
2024-06-12 16:32:25 +0900

    33

---
+++ src/components/FunctionKakaoMap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
+
+
+function FunctionKakaoMap(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default FunctionKakaoMap;
